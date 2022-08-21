8c37415c08126f735e71cbd01ced39d6a2452180
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-08-21 14:35:58 +0530

    sec-8 190. Styling & Theming App

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ShopApp
 An E-commerce app in flutter.
 An app which shows how State management concept works in flutter and how this solves the problem also we work with user input, forms, authentication and https request.
+
+models - Contains dart classes and structure of data.
+Screens - Contains the different screens of app.
+Widgets - Contains the widgets which displayed on the screens. 
--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -6,24 +6,29 @@ class ProductItem extends StatelessWidget {
   ProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: const Icon(Icons.favorite),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
       ),
     );

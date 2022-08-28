c71aedd6a8a93664dfad9803b6967a22ea182f61
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-08-28 18:29:55 +0530

    sec-8 200. Using Consumer

--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -8,21 +8,30 @@ class ProductItem extends StatelessWidget {
   //const ProductItem({Key key}) : super(key: key);
   // final String id, title, imageUrl;
   // ProductItem(this.id, this.title, this.imageUrl);
+
+  /* we can use either provider.of or consumer, here only favorite button is actully 
+  changed  when data get changed so we are using consumer there and we need data for only one time use
+  so we are using provider with listen: false*/
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context,
+        listen:
+            false); // when use provider.of it will rebuild entire build method
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black54,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            //only rebuild the part of widget tree like here only child will rebuild when data get changed
+            builder: (ctx, product, _ /*child*/) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           title: Text(
             product.title,

2ead41c31dce6cfd8d03375c99fb5472166a18a9
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-10-23 12:49:17 +0530

    sec-9 217. Snackbars & undoing action

--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -43,6 +43,19 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              //hide current scnakbar if currently on display
+              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+              //establish a connection to the nearest Scaffold
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text('Added item to cart!'),
+                duration: const Duration(seconds: 3),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),

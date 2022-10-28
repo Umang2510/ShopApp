3c0ea83207de844d81b922a8cca039bbfa0cf84b
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-10-28 13:25:34 +0530

    sec-9 227. Image Input & Image Preview

--- lib/Screens/edit_product_screen.dart
+++ lib/Screens/edit_product_screen.dart
@@ -12,53 +12,110 @@ class _EditProductScreenState extends State<EditProductScreen> {
   /// because it stick around in memory and will lead to a memory leak😣
   final _priceFocusNode = FocusNode();
   final _descFocusNode = FocusNode();
+  //implementig a listener so we do not have to press done button every time for image preview
+  final _imageUrlFocusNode = FocusNode();
+
+  //beacuse we want to access input to show preview of image before form is submitted
+  final _imageUrlController = TextEditingController();
 
   @override
-  void dispose() {
-    _priceFocusNode.dispose();
-    _descFocusNode.dispose();
-    super.dispose(); // should be at last line in dispose()
-    //while in initState super.initState should be first line
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Edit Product'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            child: ListView(
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Title'),
-                  // go to next input
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Price'),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.number,
-                  focusNode: _priceFocusNode,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_descFocusNode);
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Description'),
-                  maxLines: 4,
-                  maxLength: 150,
-                  keyboardType: TextInputType.multiline,
-                  focusNode: _descFocusNode,
-                ),
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                // go to next input
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                maxLines: 4,
+                maxLength: 150,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descFocusNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _priceFocusNode.dispose();
+    _descFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose(); // should be at last line in dispose()
+    //while in initState super.initState should be first line
   }
 }

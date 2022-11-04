3c0158f81c00c4ddd207910b112a9eedc9cfc22c
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-11-04 16:07:52 +0530

    sec-10 256. Updating "Favorite" Status

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +20,25 @@ class Product with ChangeNotifier {
       @required this.imageURL,
       this.isFavorite = false});
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.https(
+        'shopapp-5381c-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products/$id.json');
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }

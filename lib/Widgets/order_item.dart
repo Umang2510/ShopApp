b5b905a72c56b8538dbb5b0bde8d00ed98cdd6eb
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2022-12-05 16:08:44 +0530

    sec-12 289. Animating Order Boxes

--- lib/Widgets/order_item.dart
+++ lib/Widgets/order_item.dart
@@ -19,31 +19,38 @@ class _OrderItemState extends State<OrderItem> {
   //const OrderItem({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(
-              '\$${widget.order.amount.toStringAsFixed(2)}',
-              style: const TextStyle(fontWeight: FontWeight.bold),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded
+          ? min(widget.order.products.length * 20.0 + 110.0, 200.0)
+          : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                '\$${widget.order.amount.toStringAsFixed(2)}',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10.0, 100.0),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 10.0, 100.0)
+                  : 0,
               child: ListView(
                   children: widget.order.products
                       .map((prod) => Row(
@@ -65,7 +72,8 @@ class _OrderItemState extends State<OrderItem> {
                           ))
                       .toList()),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
